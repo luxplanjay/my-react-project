7c63842c5836bebf98f920adfc8ee6ff345b83ae
Alexander Repeta <alexander.repeta@gmail.com>
2025-05-11 20:46:25 +0300

    Fix problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,9 +18,9 @@ function App() {
       </div>
       <h1>Vite + React + TS</h1>
       <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button> */}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
